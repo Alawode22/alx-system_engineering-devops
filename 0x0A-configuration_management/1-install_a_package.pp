df038610cfed45c5c5883aab43efa8a5527f36e6
Alawode22 <attaurrahmanalawode@gmail.com>
2024-06-28 15:36:56 -0700

    task 0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-#!/usr/bin/pup
 # Install a version of flask (2.1.0)
+package {'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3'
+}
+
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+  require  => Package['flask']
 }
