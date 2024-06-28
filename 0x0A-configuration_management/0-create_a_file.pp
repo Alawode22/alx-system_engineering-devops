58a7a5c6a49d4b9fbb42d8ca56b7c61a686030c2
Alawode22 <attaurrahmanalawode@gmail.com>
2024-06-28 14:38:20 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # create a file in /tmp using puppet
 
 file { '/tmp/school':
-  content =>'I love Puppet',
+  ensure  => file
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'wiww-data',
+  group   => 'www-data',
 }
