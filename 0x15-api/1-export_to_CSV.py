bb53d5bacf7bd364f64addcb7399b60a8392c9d3
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-31 18:06:27 -0700

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to CSV format."""
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -26,4 +26,7 @@ if __name__ == "__main__":
     with open(file_name, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow([employee_id, username, task['completed'], task['title']])
+            writer.writerow([employee_id,
+                            username,
+                            task['completed'],
+                            task['title']])
