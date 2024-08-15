abd26b0a37c5dde2ad40a98c72e1dc7c3ad26f89
Alawode22 <attaurrahmanalawode@gmail.com>
2024-08-15 07:21:12 -0700

    sub-sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import requests
+import requests  # Import the requests module for making HTTP requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Fetch the number of subscribers for a given subreddit.
+    """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
-                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) \
+               Gecko/20100101 Firefox/68.0'}
     with requests.session() as client:
         info = client.get(url, headers=headers, allow_redirects=False).json()
         try:
