cc0d83a78760deb6ba8f2b07bd23662bc0d3c484
Alawode22 <attaurrahmanalawode@gmail.com>
2024-08-15 18:56:44 -0700

    top-10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,7 +14,7 @@ def top_ten(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'requests'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
@@ -25,4 +25,3 @@ def top_ten(subreddit):
             print(None)
     except requests.exceptions.RequestException as e:
         print(None)
-
